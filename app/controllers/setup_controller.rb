082db10de1a9c7ccc289d3cbdd85ed122b0b5e08
Yoshinori Ikarashi <yoosee@gmail.com>
2016-02-23 19:00:38 -0500

    modified setup controller. added validation to Blog model.

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -1,10 +1,20 @@
 class SetupController < ApplicationController
   def new
-    @user = User.new
-    @blog = Blog.new
+#    @user = User.new
+#    @blog = Blog.new
   end
 
   def create
+    @user = User.new params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    @blog = Blog.new params.require(:blog).permit(:title, :baseurl)
+    if @user.save
+      flash[:success] = "User successfully created."
+    else
+    end
+    if @blog.save
+    else
+    end
+    render 'new'
   end
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ActiveRecord::Base
+  validates :baseurl, presence: true, length: { maximum: 200 }, format: { with: URI::regexp(%w(http https)) }, uniqueness: { case_sensitive: false }
 end
