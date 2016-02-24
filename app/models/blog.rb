2298aaafffc9aa0cae2cef161c678a537729fb95
Yoshinori Ikarashi <yoosee@gmail.com>
2016-02-24 10:59:06 -0500

    adjust setup form. add validations to Blog model.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ActiveRecord::Base
   validates :baseurl, presence: true, length: { maximum: 200 }, format: { with: URI::regexp(%w(http https)) }, uniqueness: { case_sensitive: false }
+  validates :title, presence: true, length: { maximum: 200 }
 end
