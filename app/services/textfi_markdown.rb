e440ac8fca1c016ba1f798e72cc53278de575c3e
Yoshinori Ikarashi <yoosee@gmail.com>
2016-02-26 20:42:22 -0500

    extended Markdown (Redcarpet)

--- app/services/textfi_markdown.rb
+++ app/services/textfi_markdown.rb
@@ -3,9 +3,31 @@ class TextfiMarkdown < Redcarpet::Render::HTML
     media_id_to_url text
   end
 
-  def media_id_to_url text
+  def image link, title, alt_text
+    img = nil
+    if /id:(\d+)/ =~ link
+      media = Media.find($1)
+      img = "<img src='#{media.image.url :medium}' alt='#{alt_text}' title='#{title}'>"
+    else
+      img = "<img src='#{link}' alt='#{alt_text}' title='#{title}'>"
+    end
+    img
+  end
 
+  def media_id_to_url text
     text
   end
+
+  def header text, level
+    level += 1 # hn starts from +1 level. e.g. "#" => <h2>
+    "<h#{level}>#{text}</h#{level}>"
+  end
+
+  def block_code code, language
+    Albino.safe_colorize code, language
+  end
+
 end
+
+## markdown = Redcarpet::Markdown.new(TextfiMarkdown, :fenced_code_blocks => true)
 
