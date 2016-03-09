24a14d16355612fb3abe93c9eb538dcca3ccbd45
Yoshinori Ikarashi <yoosee@gmail.com>
2016-03-08 23:40:54 -0500

    Added Blog new/create, edit/update and index.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,43 @@
 class BlogsController < ApplicationController
+  before_action :signed_in_user
   def new
     @blog = Blog.new
   end
 
+  def index
+    @blogs = Blog.all
+  end
+
+  def create
+    @blog = Blog.new blog_params
+    if @blog.save
+      flash[:success] = "New Blog Created"
+    else
+      flash[:danger] = "New Blog could not be created."
+      render 'new'
+    end
+    render 'index'
+  end
+
+  def edit
+    @blog = Blog.find params[:id]
+    render 'edit'
+  end
+
+  def update
+    @blog = Blog.find params[:id]
+    if @blog.update_attributes blog_params
+      flash[:success] = "Blog updated"
+      redirect_to 'index'
+    else
+      flash[:danger] = "Blog update failed"
+      render 'edit'
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :baseurl, :summary)
+  end
 
 end
