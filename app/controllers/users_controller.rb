4aeaa9a4a0602c870542d2402385dfcf7a322d71
Yoshinori Ikarashi <yoosee@gmail.com>
2016-03-09 20:13:39 -0500

    Fix new user signup access control.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:edit, :update, :new]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
