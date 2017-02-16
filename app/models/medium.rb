6b35a12a8f37ac18fbd752f055efb5fab1af3831
Yoshinori Ikarashi <yoosee@gmail.com>
2017-02-16 09:29:21 -0500

    updated rails version to 4.2.7.1 added quality option to paperclip (medium.rb)

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -4,7 +4,9 @@ class Medium < ActiveRecord::Base
   has_attached_file :image,
     styles: { medium: "920x480>", thumb: "120x120>" },
     convert_options: { 
-      all: "-strip"
+      all: "-strip",
+      medium: "-quality 65",
+      thumb: "-quality 60"
     },
     default_url: "/image/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
