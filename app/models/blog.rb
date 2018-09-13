5e4d4360415603aeef6f146e16e4e9409b393972
Yoshinori Ikarashi <yoosee@gmail.com>
2018-09-13 07:31:01 +0000

    Modify ActiveRecord::Base to ApplicationRecord and also Job

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
-class Blog < ActiveRecord::Base
+class Blog < ApplicationRecord
   validates :baseurl, presence: true, length: { maximum: 200 }, format: { with: URI::regexp(%w(http https)) }, uniqueness: { case_sensitive: false }
   validates :title, presence: true, length: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_many :articles
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
