cfa986a81c164a245cb2af82a3583c3ba74cce65
yoosee <yoosee@gmail.com>
2021-04-12 01:53:33 +0000

    Applied Rails 5.2.5 rake ap:update and fixed errors.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,10 +27,9 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-  config.action_mailer.perform_caching = false
 
   # Store uploaded files on the local file system in a temporary directory
-#  config.active_storage.service = :test
+  config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
 
