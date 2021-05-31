5a352457115d6dfa164d03b227186f35404f32a0
yoosee <yoosee@gmail.com>
2021-05-31 01:18:20 +0000

    Apply Host Authorization Whitelist for dev and prod environment files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Whitelsit specific hosts
+#  config.hosts << "hostname"
+  # remove restriction of hostname and accept requests for all hostnames. (development only)
+  config.hosts.clear
+
 end
